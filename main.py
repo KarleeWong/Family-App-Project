d198c88465ee726cc45688231a7eeb7ea60c1dfa
Ghislaib <39511515+Ghislaib@users.noreply.github.com>
2018-07-29 14:20:48 -0700

    It worked

---
+++ main.py
@@ -0,0 +1,27 @@
+import webapp2
+import jinja2
+import os
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+
+class EnterInfoHandler(webapp2.RequestHandler):
+    def get(self):
+        frontpage_template = JINJA_ENVIRONMENT.get_template('templates/frontpage.html')
+        self.response.write(frontpage_template.render())
+
+
+# class ImageImporter(webapp2.RequestHandler):
+#     def get(self):
+#         frontpage_template = JINJA_ENVIRONMENT.get_template('pages_outline/frontpage.html')
+#         self.response.write(frontpage_template.render())
+#     def post(self):
+#         self.response.write('hi')
+
+app = webapp2.WSGIApplication([
+    ('/', EnterInfoHandler),
+    # ('/', ImageImporter)
+], debug=True)
