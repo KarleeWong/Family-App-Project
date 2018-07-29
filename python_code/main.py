cfa0e4009017bc3aeb6f704e6ca482cc55b58279
J Bhamipuri <jsnanak@gmail.com>
2018-07-29 12:56:22 -0700

    NEw stuff
    trying and changning things get the local host to work

--- python_code/main.py
+++ python_code/main.py
@@ -6,11 +6,12 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__))
 
 class ImageImporter(webapp2.RequestHandler):
-
     def get(self):
-        frontpage_template = JINJA_ENVIRONMENT.get_template('Styling/frontpage.html')
+        frontpage_template = JINJA_ENVIRONMENT.get_template('pages_outline/frontpage.html')
         self.response.write(frontpage_template.render())
+    def post(self):
+        self.response.write('hi')
 
 app = webapp2.WSGIApplication([
-    ('/II', ImageImporter)
+    ('/fam', ImageImporter)
 ], debug=True)
