efc5ac2f032cc44859cbdc9834a2fcdeb3dc3192
Ghislaib <39511515+Ghislaib@users.noreply.github.com>
2018-07-29 14:09:45 -0700

    Trying to connect
    still trying to connect the html file and python

--- python_code/main.py
+++ python_code/main.py
@@ -3,7 +3,16 @@ import jinja2
 import os
 
 JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__))
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+
+class EnterInfoHandler(webapp2.RequestHandler):
+    def get(self):
+        Welcome_template = JINJA_ENVIRONMENT.get_template('templates/frontpage.html')
+        self.response.write(frontpage_template.render())
+
 
 class ImageImporter(webapp2.RequestHandler):
     def get(self):
@@ -13,5 +22,6 @@ class ImageImporter(webapp2.RequestHandler):
         self.response.write('hi')
 
 app = webapp2.WSGIApplication([
+    ('/ok', EnterInfoHandler),
     ('/', ImageImporter)
 ], debug=True)
