cb446e82bab0ab7e854b61e3bbd442e336bba248
Karlee <Karlee.L.Wong@gmail.com>
2018-07-29 12:21:51 -0700

    Fixes to python code to HOPEFULLY run it

--- python_code/main.py
+++ python_code/main.py
@@ -3,10 +3,7 @@ import jinja2
 import os
 
 JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
-    extensions=['jinja2.ext.autoescape'],
-    autoescape=True)
-
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__))
 
 class ImageImporter(webapp2.RequestHandler):
     def post(self):
