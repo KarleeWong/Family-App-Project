ede54413d44cf5d2430be00e42c9c9314a986dc6
Karlee <Karlee.L.Wong@gmail.com>
2018-07-31 09:29:24 -0700

    Adding Model.py classes

--- model.py
+++ model.py
@@ -1,1 +1,11 @@
-#Python
+from google.appengine.ext import ndb
+
+class Family_Member(ndb.Model):
+   name = ndb.StringProperty(required=True)
+   description = ndb.StringProperty(required=True)
+   type = ndb.StringProperty(required=True)
+
+class Picture(ndb.Model):
+   title = ndb.StringProperty(required=True)
+   url = ndb.StringProperty(required=True)
+   tags = ndb.StringProperty(required=True)
